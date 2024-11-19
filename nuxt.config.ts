33d8a441232973fa75c74103a0296403f50c5d93
kkm4512 <nayoun440@naver.com>
2024-11-19 20:59:23 +0900

    https 추가

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,8 +13,8 @@ export default defineNuxtConfig({
     public: {
       baseFrontUrl: 'http://localhost:3000',
       baseWebSocketUrl: 'doguin-alb-1242367005.ap-northeast-2.elb.amazonaws.com',
-      baseApi: 'https://doguin-alb-1242367005.ap-northeast-2.elb.amazonaws.com/api/v1', // API 베이스 URL 등도 여기에 정의할 수 있어
-      baseTest: 'https://doguin-alb-1242367005.ap-northeast-2.elb.amazonaws.com',
+      baseApi: 'https://dogu-in.com/api/v1', // API 베이스 URL 등도 여기에 정의할 수 있어
+      baseTest: 'https://dogu-in.com',
       kakaoLoginUrl: 'https://kauth.kakao.com/oauth/authorize?client_id=4c20fa460462cdcce6dc2f520464610e&redirect_uri=http://doguin-alb-1242367005.ap-northeast-2.elb.amazonaws.com/api/v1/auth/oauth2/authorize/kakao&response_type=code',
       naverLoginUrl: 'https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=105NIEggD1Vv4xj2p90y&state=YOUR_GENERATED_STATE&redirect_uri=http://doguin-alb-1242367005.ap-northeast-2.elb.amazonaws.com/api/v1/auth/oauth2/authorize/naver',
       githubLoginUrl: 'https://github.com/login/oauth/authorize?client_id=Ov23liUpy59bQXa5zJZj&redirect_uri=http://doguin-alb-1242367005.ap-northeast-2.elb.amazonaws.com/api/v1/auth/oauth2/authorize/github&scope=user:email',
