7b37134f653475d678c667533a8f6197e4ab431e
kkm4512 <nayoun440@naver.com>
2024-11-19 22:19:41 +0900

    웹소켓 해결

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,7 +12,7 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       baseFrontUrl: 'http://localhost:3000',
-      baseWebSocketUrl: 'doguin-alb-1242367005.ap-northeast-2.elb.amazonaws.com',
+      baseWebSocketUrl: 'dogu-in.com',
       baseApi: 'https://dogu-in.com/api/v1', // API 베이스 URL 등도 여기에 정의할 수 있어
       baseTest: 'https://dogu-in.com',
       kakaoLoginUrl: 'https://kauth.kakao.com/oauth/authorize?client_id=4c20fa460462cdcce6dc2f520464610e&redirect_uri=http://doguin-alb-1242367005.ap-northeast-2.elb.amazonaws.com/api/v1/auth/oauth2/authorize/kakao&response_type=code',
